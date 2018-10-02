f32ab1273b79638eaf4f813027eaf85f6f8b92ef
Sadanand Vishwas <31150629+saddhu1005@users.noreply.github.com>
2018-10-02 23:14:28 +0530

    Create hash_things_Mo.cpp

---
+++ hash_things_Mo.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define mxn 10005
+#define mxq 500007
+/*ll p[11];
+int block_size;
+long long ans[mxq];
+//map<ll,vector<int> >mp;
+
+struct value
+{
+    ll hashval;
+    string str;
+};
+value initial[mxn];
+struct queries
+{
+    int li;
+    int ri;
+    ll hashval;
+    string str;
+    int id;
+};
+queries query[mxq];
+void hashkey()
+{
+    p[10]=31;
+   p[0]=1;
+   for(ll i=1;i<10;++i)
+    p[i]=p[i-1]*p[10];
+}
+ll geth(string st)
+{
+    ll ret=0,i;
+    for( i=0;st[i];++i)
+    {
+        ret+=(st[i]-'a'+1)*p[i];
+        ret%=1000000009;
+    }
+    return ret;
+}
+inline bool cmp(queries q1,queries q2)
+{
+  q1.li/=block_size;
+  q2.li/=block_size;
+  if(q1.li!=q2.li)
+  return q1.li<q2.li;
+  return q1.ri<q2.ri;
+}
+bool isstr()
+{
+    
+}
+*/
+int main()
+{
+cout<<"LOL"<<"\n";
+return 0;
